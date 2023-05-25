1d08928f665dedffdb7744f63bd51bce9da20947
Vrrl <nykogabriel@gmail.com>
2023-05-25 09:16:43 -0300

    Fixing layout & style inconsistency

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -11,21 +11,19 @@ interface HomeProps {
  
 const Home = () => {
     return (
-        <div>
-            <div className={styles.page}>
-            <Header/>
-                <div className={styles.content}>
-                    <div className={styles.msg}>
-                        <h1><b>Find</b> all your favorite <b>Pokemon</b></h1>
-                        <p>You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
-                        <button>See Pokemons</button>
-                    </div>
-                    <div className={styles.img}>
-                        <img src={pikachuImg}></img>
-                    </div>
+        <div className={styles.page}>
+        <Header/>
+            <div className={styles.content}>
+                <div className={styles.msg}>
+                    <h1><b>Find</b> all your favorite <b>Pokemon</b></h1>
+                    <p>You can know the type of Pokemon, its strengths, disadvantages and abilities</p>
+                    <button>See Pokemons</button>
                 </div>
-            <Footer/>
+                <div className={styles.img}>
+                    <img src={pikachuImg}></img>
+                </div>
             </div>
+        <Footer/>
         </div>
     );
 }
